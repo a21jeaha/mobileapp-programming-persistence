328b0f70d82cb1ac5505266361e5a0051416a7e1
Jean-Paul <a21jeaha@student.his.se>
2022-05-10 13:03:52 +0200

    tre textviews added to layout, giving table names to the edittext widgets

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button readButton;
+    Button writeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readButton = findViewById(R.id.Read_Button);
+        writeButton = findViewById(R.id.Write_Button);
+
+        readButton.setOnClickListener(new View.OnClickListener() {        // GÖR KLART SENARE
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        writeButton.setOnClickListener(new View.OnClickListener() {        // GÖR KLART SENARE
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
