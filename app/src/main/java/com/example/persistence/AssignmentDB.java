3f37b9f06f40a5a4755912cf7ae420f5ff872f52
Jean-Paul <a21jeaha@student.his.se>
2022-05-10 15:49:38 +0200

    Foundation for data base created

---
+++ app/src/main/java/com/example/persistence/AssignmentDB.java
@@ -0,0 +1,39 @@
+package com.example.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+import androidx.annotation.Nullable;
+
+public class AssignmentDB extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "person.db";                        // måste vara static för att fungera
+    private static final int DATABASE_VERSION = 1;
+    //
+    public static final String TABLE_PEOPLE = "people";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAMN = "namn";
+    public static final String COLUMN_AGE = "age";
+    public static final String COLUMN_HIGHT = "hight";
+
+    public AssignmentDB(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {                           //// refractor -- constant
+        db.execSQL("CREATE TABLE " + TABLE_PEOPLE + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                    COLUMN_NAMN + " TEXT, " +
+                    COLUMN_AGE + " INTEGER, " +
+                    COLUMN_HIGHT + " INTEGER )");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
